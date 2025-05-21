64ed3da45d430512397d6035e4edccf17b490ab1
Palegg Works <palegg.works@gmail.com>
2025-05-20 22:58:28 -0400

    update icon

--- src/components/header_navbar.rs
+++ src/components/header_navbar.rs
@@ -1,12 +1,19 @@
 use super::routes::Route;
 use dioxus::prelude::*;
 
+const ICON_ALL_TASKS: Asset = asset!("/assets/png/all_tasks.png");
+const ICON_NEW_TASKS: Asset = asset!("/assets/png/new_task.png");
+const ICON_LOG_ACTION: Asset = asset!("/assets/png/log_action.png");
+const ICON_QA: Asset = asset!("/assets/png/about.png");
+
 #[component]
 pub fn HeaderNavbar() -> Element {
     let current_route = use_route::<Route>();
     let is_active = |target: &Route| current_route == *target;
 
-    let tab_css = "flex justify-center px-2 py-5 text-blue-700 font-medium";
+    //let tab_css = "flex justify-center px-2 py-5 text-blue-700 font-medium";
+    let tab_css = "flex justify-center items-center gap-2 px-2 py-5 text-blue-700 font-medium";
+    let icon_css = "w-7 h-7";
 
     let active_class_str =
         "bg-white border border-gray-300 rounded-md shadow-sm text-blue-900 hover:text-blue-900";
@@ -21,7 +28,15 @@ pub fn HeaderNavbar() -> Element {
                 Link {
                     class: tab_css,
                     to: Route::TaskList,
-                    "📋 All Tasks"
+                    img {
+                        src: ICON_ALL_TASKS,
+                        class: icon_css,
+                        alt: "All Tasks"
+                    },
+                    span {
+                        class: "hidden sm:inline",
+                        " All Tasks"
+                    }
                 }
             }
 
@@ -30,7 +45,15 @@ pub fn HeaderNavbar() -> Element {
                 Link {
                     class: tab_css,
                     to: Route::TaskCreate,
-                    "🆕 New Task"
+                    img {
+                        src: ICON_NEW_TASKS,
+                        class: icon_css,
+                        alt: "New Task"
+                    },
+                    span {
+                        class: "hidden sm:inline",
+                        " New Task"
+                    }
                 }
             }
 
@@ -39,7 +62,15 @@ pub fn HeaderNavbar() -> Element {
                 Link {
                     class: tab_css,
                     to: Route::ActionLog,
-                    "📝 Log Action"
+                    img {
+                        src: ICON_LOG_ACTION,
+                        class: icon_css,
+                        alt: "Log Action"
+                    },
+                    span {
+                        class: "hidden sm:inline",
+                        " Log Action"
+                    }
                 }
             }
 
@@ -48,7 +79,15 @@ pub fn HeaderNavbar() -> Element {
                 Link {
                     class: tab_css,
                     to: Route::About,
-                    "🧐 Q&A"
+                    img {
+                        src: ICON_QA,
+                        class: icon_css,
+                        alt: "Q&A"
+                    },
+                    span {
+                        class: "hidden sm:inline",
+                        " Q&A"
+                    }
                 }
             }
 
