97578158b0f4de15134bf8345cc69f5c38430b7c
Palegg Works <palegg.works@gmail.com>
2025-05-29 23:29:49 -0400

    update android build logic

--- build_android.sh
+++ build_android.sh
@@ -6,26 +6,33 @@
 # My icons were made using this tool
 # https://icon.kitchen/
 
+# Before running this code, make sure you have:
+#
+# 1. Set the KEYSTORE_PASS variable
+# 2. Generated a jks file with the following code
+#    or a jks file has already been created with the KEYSTORE_PASS
+: '
+keytool -genkeypair -v \
+  -keystore .release_android_key.jks -keyalg RSA \
+  -keysize 2048 -validity 10000 -alias ClockMobile \
+  -storepass $KEYSTORE_PASS \
+  -dname "CN=Weiming Hu, OU=Dev, O=Weiming Hu, L=Harrisonburg, C=US"
+'
+
 set -e
 VERSION=$(grep '^version' Cargo.toml | sed -E 's/version *= *"(.*)"/\1/')
 
+
+if ! [ -f .release_android_key.jks ]; then
+  echo "❌ Android release key does not exist."
+  echo "Read the start of build_android.sh to learn how to generate this file"
+  echo "This file should be reused across different builds and versions"
+  exit 1
+fi
+
 if [[ -z "$KEYSTORE_PASS" ]]; then
   echo "❌ KEYSTORE_PASS is not set."
-  echo "ℹ️ Attempting to source .env..."
-
-  # Source .env if it exists
-  if [[ -f .env ]]; then
-    source .env
-  else
-    echo "❌ .env file not found."
-    exit 1
-  fi
-
-  # Check again after sourcing
-  if [[ -z "$KEYSTORE_PASS" ]]; then
-    echo "❌ KEYSTORE_PASS is still not set. Please define it in your environment or .env file."
-    exit 1
-  fi
+  exit 1
 fi
 
 # bundle with dx
@@ -42,12 +49,6 @@ cp -r ../../../../../../assets/icons/android/res app/src/main/
 rm app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml
 ./gradlew assembleRelease
 
-keytool -genkeypair -v \
-  -keystore release_android_key.jks -keyalg RSA \
-  -keysize 2048 -validity 10000 -alias ClockMobile \
-  -storepass $KEYSTORE_PASS \
-  -dname "CN=Weiming Hu, OU=Dev, O=Weiming Hu, L=Harrisonburg, C=US"
-
 # optimize the apk
 zipalign -v -p 4 \
     app/build/outputs/apk/release/app-release-unsigned.apk \
@@ -55,7 +56,7 @@ zipalign -v -p 4 \
 
 # sign and install
 apksigner sign \
-    --ks release_android_key.jks \
+    --ks .release_android_key.jks \
     --ks-pass pass:$KEYSTORE_PASS \
     --out app-release-signed.apk \
     app-release-aligned.apk
@@ -63,7 +64,6 @@ apksigner sign \
 # if you dont need install, you can change it to mv it below workspace.
 # adb install -r app-release-signed.apk
 
-rm release_android_key.jks
 rm app-release-aligned.apk
 zip stay-ahead.zip app-release-signed.apk
 cp stay-ahead.zip ~/Desktop/StayAhead_"$VERSION"_Android.zip
